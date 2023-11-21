42f3cb0bb4008bcdfb67f5152f92fdf7f246c57d
Mark <mr.assejev@gmail.com>
2023-11-21 14:03:47 +0100

    new files

--- GenerateImages.py
+++ GenerateImages.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import shutil
 
 # Load the .npy file
-file_path = r'MLProject23/fashion_train.npy'  # Replace with your .npy file path
+file_path = 'MLProject23/fashion_train.npy'  # Replace with your .npy file path
 
-# Now, let's load the dummy .npy file as if it was the actual dataset
+# Load the data
 data = np.load(file_path)
 
 # Separate the images from the labels
@@ -15,26 +14,20 @@ images, labels = data[:, :-1], data[:, -1]
 # Reshape the images for display
 images_reshaped = images.reshape(-1, 28, 28)
 
-# Display the shape of the dataset and the first few images
-data_shape = images_reshaped.shape
-
-# Show the first few images in the dataset
-#fig, axes = plt.subplots(1, 5, figsize=(10, 2))
-#for i, ax in enumerate(axes):
-#    ax.imshow(images_reshaped[i], cmap='gray')
-#    ax.axis('off')
-#plt.show()
-
-#data_shape, labels[:5]  # Display the first five labels for reference
-
+# Create a directory to store the images, change this to your preferred location
 export_dir = 'exported_images'
-if os.path.exists(export_dir):
-    shutil.rmtree(export_dir)
+if not os.path.exists(export_dir):
+    os.makedirs(export_dir)
 
-# Recreate the directory
-os.makedirs(export_dir)
+# Create subdirectories for each class and save images
+for class_id in np.unique(labels):
+    class_dir = os.path.join(export_dir, f'class_{int(class_id)}')
+    os.makedirs(class_dir, exist_ok=True)
 
-# Save the first 1000 images as .jpg files
-for i in range(1000):
-    image_path = os.path.join(export_dir, f'image_{i+1}.jpg')
-    plt.imsave(image_path, images_reshaped[i], cmap='gray')
+    # Filter images for this class
+    class_images = images_reshaped[labels == class_id]
+
+    # Save the first N images of this class
+    for i in range(min(1000, len(class_images))):
+        image_path = os.path.join(class_dir, f'image_{i+1}.jpg')
+        plt.imsave(image_path, class_images[i], cmap='gray')
